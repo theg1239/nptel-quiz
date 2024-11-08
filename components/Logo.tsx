e230ae8795c6e8c478e3cd5792ec07b47439cf63
theg <52027622+theg1239@users.noreply.github.com>
2024-11-08 22:17:09 +0530

    opti

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import React from 'react'
+
 const Logo = () => (
-  <div className="flex justify-center items-center min-h-[100px] text-4xl font-bold">
+  <div className="flex justify-center items-center min-h-[100px] text-4xl font-bold fade-in">
     <span>
       <span>NPTEL</span>
       <span className="text-transparent bg-clip-text bg-gradient-to-tr from-[#253EE0] to-[#27BAEC]">
